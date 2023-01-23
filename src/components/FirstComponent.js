e42d9e9403820ba2fc891972a5834beeb5ecca85
Kayky Morais <kaykygithub@gmail.com>
2023-01-22 23:44:38 -0300

    Fundamentos - Aula 23

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyComponent from './MyComponent'
+
+const FirstComponent = () => {
+  return (
+    <div>
+        <h1>My first component</h1>
+        <MyComponent/>
+    </div>
+  )
+}
+
+export default FirstComponent
