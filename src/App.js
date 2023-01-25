e6304aced1c2576421e15f13f25e4fb263e09293
Kayky Morais <kaykygithub@gmail.com>
2023-01-24 22:13:51 -0300

    Fundamentos do react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import FirstComponent from './components/FirstComponent';
 import './App.css';
 import TemplateExpression from './components/TemplateExpression';
-
+import Events from './components/Events';
+import Challenge from './components/Challenge';
 
 function App() {
   return (
     <div className="App">
       <FirstComponent/>
       <TemplateExpression/>
+      <Events/>
+      <Challenge/>
     </div>
   );
 }
